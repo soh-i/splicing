8a7e49aa59d8281f9f452ae826cd99626c2c5655
soh-i <soh.ishiguro3914@gmail.com>
2012-09-04 21:22:56 +0900

    Add loading gtf file and its attribute column ware substituted by each value

--- find_deletion.pl
+++ find_deletion.pl
@@ -7,8 +7,10 @@ use 5.12.4;
 use Readonly;
 use Data::Dumper;
 
+### COMMAND LINE
 if ( scalar @ARGV != 0 ) {
     my $cmd = join ",", @ARGV;
+    
     if ( $cmd =~ m/^-{2}help$/ || $cmd =~ m/^\-{1}h$/ ) {
         say help();
         exit(1);
@@ -24,9 +26,10 @@ if ( scalar @ARGV != 0 ) {
 my $editing_list = '/home/soh.i/melanogaster/DARNED_dm3.txt';
 my $annotation_gtf = '/home/soh.i/melanogaster/Ensembl.genes.gtf';
 
-# Loading 
+# Loading DARNED file
 open my $list_fh, '<', $editing_list || die;
 my $list = {};
+
 while ( my $entry = <$list_fh> ) {
     next if $entry =~ m/^chrom\s+coordinate/;
     chomp $entry;
@@ -63,18 +66,61 @@ while ( my $entry = <$list_fh> ) {
 }
 close $list_fh;
 
-print Dumper $list;
+#print Dumper $list;
 
-
-
-
-
+# Loading dm3 gtf file
 open my $anno_fh, '<', $annotation_gtf || die;
 my $gtf = {};
-while ( my $line = <$anno_fh> ) {
-}
 
+while ( my $line = <$anno_fh> ) {
+    
+    next if $line =~ /#+/;
+    next if !$line =~ /exon/;
+    
+    my @t = split /\t/, $line;
+    my $chromosome = $t[0];
+    my $source     = $t[1];
+    my $feature    = $t[2];
+    my $start      = $t[3];
+    my $end        = $t[4];
+    my $score      = $t[5];
+    my $strand     = $t[6];
+    my $frame      = $t[7];
+    my $attribute  = $t[8];
+    
+    my @atr = split /\;/, $attribute;
+    (my $gene_id         = $atr[0]) =~ s/["]+//g;
+    (my $transcript_id   = $atr[1]) =~ s/["\s]+//g;
+    (my $exon_number     = $atr[2]) =~ s/["\s]+//g;
+    (my $gene_name       = $atr[3]) =~ s/["\s]+//g;
+    (my $p_id            = $atr[4]) =~ s/["\s]+//g;
+    (my $seqedit         = $atr[5]) =~ s/["\s]+//g;
+    (my $transcript_name = $atr[6]) =~ s/["\s]+//g;
+    (my $tss_id          = $atr[7]) =~ s/["\s]+//g;
+
+=pod    
+    say $gene_id;
+    say $transcript_id;
+    say $exon_number;
+    say $p_id;
+    say $seqedit;
+    say $transcript_name;
+    say $tss_id;
+    <STDIN>;
+=cut
+
+    my $gtf_primary = q//;
+    Readonly $gtf_primary => "$start-$end-$transcript_id-$exon_number";
+    die $gtf_primary;
+}
 close $anno_fh;
+
+
+foreach my $gtf ( keys %{ $gtf } ) {
+    foreach my $editing ( keys %{ $list } ) {
+        
+    }
+}
 
 
 ### HELP MASSAGES
